39ba077edb6fb88418aded3420a29b15e25b1ded
WBs234 <96954406+WBs234@users.noreply.github.com>
2022-10-03 23:38:57 -0300

    Update Banscript.py

--- Banscript.py
+++ Banscript.py
@@ -6,7 +6,6 @@ print("Olá, bem vindo ao script de dar ban em números :)")
 num=int(input("Digite o número que você deseja dar ban: "))
 print('\n')
 resp=int(input(f"O número digitado foi {num}, está correto?[1=Y/2=N] "))
-gay()
 if resp==1:
     os.system("cmatrix")
 else:

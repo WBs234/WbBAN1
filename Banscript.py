dcba371d5030920a42e1e3c9e880047dc69a406b
WBs234 <96954406+WBs234@users.noreply.github.com>
2022-10-03 23:36:48 -0300

    Update Banscript.py

--- Banscript.py
+++ Banscript.py
@@ -2,11 +2,10 @@
 import os
 import time
 
-def gay():
-     print("Olá, bem vindo ao script de dar ban em números :)")
-     num=int(input("Digite o número que você deseja dar ban: "))
-     print('\n')
-     resp=int(input(f"O número digitado foi {num}, está correto?[1=Y/2=N] "))
+print("Olá, bem vindo ao script de dar ban em números :)")
+num=int(input("Digite o número que você deseja dar ban: "))
+print('\n')
+resp=int(input(f"O número digitado foi {num}, está correto?[1=Y/2=N] "))
 gay()
 if resp==1:
     os.system("cmatrix")
